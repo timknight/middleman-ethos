1a55e64e1c58de304354e0929b162e77d198d76f
Tim Knight <tim.knight@acst.org>
2014-07-20 18:20:55 -0400

    Added base layout, javascript, Sass, and improved helpers

--- README.markdown
+++ README.markdown
@@ -1,3 +1,7 @@
 # Middleman Ethos
 
-A basic prototyping framework for Middleman in early development.
+A  prototyping framework for Middleman in early development. Currently using Bootstrap 3 and jQuery, but this might change.
+
+## Project Configuration
+
+Project information is found in /data/config.yml
--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,15 +1,20 @@
 module SiteHelpers
 
-  def current(nav)
-    nav == current_page.data.nav
+  # TODO: Get this supporting the subnav as well.
+  def current?(nav)
+    nav == data.page.nav
   end
 
   def page_title
-    if current_page.data.title
-      "#{current_page.data.title} - #{data.config.title}"
+    if data.page.title
+      "#{data.page.title} - #{data.config.title}"
     else
       project.data.title
     end
+  end
+
+  def page_description
+    content_tag :meta, "", {name: "description", value: data.page.description } if data.page.description
   end
 
 end
